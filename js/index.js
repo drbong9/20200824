7f2876b2de2f933a00109f0f629ae07a732db6d4
drbong9 <52834463+drbong9@users.noreply.github.com>
2020-09-11 22:22:24 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,43 @@
+/********** 사전지식 **********/
+
+
+/********** 전역변수 **********/
+var isWingShow = false;
+
+/********** 사용자정의 **********/
+
+
+/********** 이벤트콜백 **********/
+function onWingClick(){
+    if(isWingShow){
+        isWingShow = false;
+        $('.wing-conts').css('transform','translateX(350px)');
+        $('.wing-wrap').css('background-color','rgba(0,0,0,0)');      
+        setTimeout(function(){
+            $('.wing-wrap').css('display','none');
+        },300);
+    }
+    else {
+        isWingShow = true;
+        $('.wing-wrap').css('display','block');
+        setTimeout(function(){
+            $('.wing-wrap').css('background-color','rgba(0,0,0,0.4)');
+        },0);
+        setTimeout(function(){
+            $('.wing-conts').css('transform','translateX(0)');
+        },300);
+       }
+}
+function onResize(){
+    if($(this).outerWidth() >= 768){
+        isWingShow = true;
+        onWingClick();
+    }
+    if($(this).outerWidth() < 768){
+        
+    }
+}
+
+/********** 이벤트등록 **********/
+$('.bt-wing').click(onWingClick);
+$(window).resize(onResize);
