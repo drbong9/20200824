f3cf12cbf8decd9b1a8235cf8d251a5c32cc59aa
drbong9 <52834463+drbong9@users.noreply.github.com>
2020-09-12 20:05:55 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -3,9 +3,37 @@
 
 /********** 전역변수 **********/
 var isWingShow = false;
+var mainNow = 0;
+var $mainSlide = $(".main-wrap .slide");
+var mainLast = $mainSlide.length - 1;
+var mainSpeed = 500;
+var mainGap = 3000;
+var mainInterval;
+var mainPager = {off:'○',on:'●'};
+mainInit();
+mainPagerInit();
+onMainLeave();
 
 /********** 사용자정의 **********/
-
+function mainInit(){
+    $('.slides').empty();
+    $($mainSlide[mainNow]).appendTo('.main-wrap .slides').removeClass('slide');
+}
+function mainAni(){
+    $('.main-wrap .pager').html(mainPager.off);
+    $('.main-wrap .pager').eq(mainNow).html(mainPager.on);
+    $($mainSlide[mainNow]).prependTo('.main-wrap .slides').css('opacity',0)
+    .addClass('slide').stop()
+    .animate({'opacity': 1},mainSpeed,mainInit);
+}
+function mainPagerInit(){
+    for(var i=0,html; i<=mainLast; i++){ 
+        if(mainNow == i) html = '<span class="pager">'+mainPager.on+'</span>';
+        else html = '<span class="pager">'+mainPager.off+'</span>';      
+        $(html).appendTo('.main-wrap .pagers').click(onPager);
+    }
+           
+}
 
 /********** 이벤트콜백 **********/
 function onWingClick(){
@@ -37,7 +65,28 @@ function onResize(){
         
     }
 }
+function onPrev(){
+    mainNow = (mainNow == 0) ? mainLast : mainNow - 1;
+    mainAni();
+}
+function onNext(){
+    mainNow = (mainNow == 3) ? 0 : mainNow + 1;
+    mainAni();
+}
+function onPager(){
+    mainNow = $(this).index();
+    mainAni();
+}
+function onMainHover(){
+    clearInterval(mainInterval);
+}
+function onMainLeave(){
+    mainInterval = setInterval(onNext,mainGap);
+}
 
 /********** 이벤트등록 **********/
 $('.bt-wing').click(onWingClick);
 $(window).resize(onResize);
+$('.main-wrap .bt-prev').click(onPrev);
+$('.main-wrap .bt-next').click(onNext);
+$('.main-wrap').hover(onMainHover,onMainLeave);
